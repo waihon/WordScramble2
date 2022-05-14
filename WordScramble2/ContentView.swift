c200716a5d004653203dc0d823df1828d3eae2a2
Waihon Yew <yewwaihon@gmail.com>
2022-05-14 23:48:57 +0800

    Promote ListView to be same level as ContentView

--- WordScramble2/ContentView.swift
+++ WordScramble2/ContentView.swift
@@ -7,31 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    static let listStyleTexts = ["Automatic", "Grouped", "Inset", "Inset Grouped", "Plain", "Sidebar"]
-    @State private var listStyleText = listStyleTexts[0]
+struct ListView: View {
+    var body: some View {
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
 
-    struct ListView: View {
-        var body: some View {
-            List {
-                Section("Section 1") {
-                    Text("Static row 1")
-                    Text("Static row 2")
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
                 }
+            }
 
-                Section("Section 2") {
-                    ForEach(0..<5) {
-                        Text("Dynamic row \($0)")
-                    }
-                }
-
-                Section("Section 3") {
-                    Text("Static row 3")
-                    Text("Static row 4")
-                }
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
             }
         }
     }
+}
+
+struct ContentView: View {
+    static let listStyleTexts = ["Automatic", "Grouped", "Inset", "Inset Grouped", "Plain", "Sidebar"]
+    @State private var listStyleText = listStyleTexts[0]
 
     var body: some View {
         VStack {
