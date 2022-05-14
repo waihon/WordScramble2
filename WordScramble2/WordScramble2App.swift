0d7bb3e896bd8fbf949c279978710edf5cb68ea4
Waihon Yew <yewwaihon@gmail.com>
2022-05-14 10:40:14 +0800

    Initial Commit

---
+++ WordScramble2/WordScramble2App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble2App.swift
+//  WordScramble2
+//
+//  Created by Waihon Yew on 14/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
