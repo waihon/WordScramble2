5a6cce36d06b6adb4aca20c1d8e7622f5d685930
Waihon Yew <yewwaihon@gmail.com>
2022-05-14 11:12:08 +0800

    Use sections to make our list easier to read

--- WordScramble2/ContentView.swift
+++ WordScramble2/ContentView.swift
@@ -10,6 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+    }
+}
+
+struct List03ContentView: View {
+    var body: some View {
+        List {
             Text("Static row 1")
             Text("Static row 2")
 
